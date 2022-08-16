e330c76f92858cd6ec50b57f04975b4f713b2a9a
Grimlock8 <105064843+Grimlock8@users.noreply.github.com>
2022-08-16 11:18:30 -0500

    Added Download function

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ var track = null;
 const cameraView = document.querySelector("#camera--view"),
     cameraOutput = document.querySelector("#camera--output"),
     cameraSensor = document.querySelector("#camera--sensor"),
-    cameraTrigger = document.querySelector("#camera--trigger");
+    cameraTrigger = document.querySelector("#camera--trigger"),  
+    downloadTrigger = document.querySelector("#downloadPhoto");
 
 // Access the device camera and stream to cameraView
 function cameraStart() {
@@ -29,6 +30,12 @@ cameraTrigger.onclick = function() {
     cameraOutput.src = cameraSensor.toDataURL("image/webp");
     cameraOutput.classList.add("taken");
     // track.stop();
+};
+
+downloadTrigger.onclick = function() {
+    const canvas = cameraSensor.toDataURL("image/jpeg")
+    .replace("image/jpeg", "image/octet-stream");    
+    downloadTrigger.setAttribute("href", canvas);
 };
 
 // Start the video stream when the window loads
